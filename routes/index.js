b9ead03231b5fcd382efcd8367bd40c881ba32d2
Satyam <msatyam737@gmail.com>
2023-07-26 19:06:42 +0530

    adding view engine

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const homecontroller = require('..//controller/home_controller');
 
 router.get('/',homecontroller.home);
-
+router.use('/user', require('./user'));
 console.log('router loaded');
 
 module.exports=router;
